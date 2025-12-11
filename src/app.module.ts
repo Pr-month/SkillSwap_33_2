6ea27a457fce8300c29a0f5d631b839d7639af03
Elena Romanova <romanova.bim@gmail.com>
2025-12-12 01:01:18 +0500

    cоздать конфиг для приложения app.config.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './config/app.config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule, 
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
