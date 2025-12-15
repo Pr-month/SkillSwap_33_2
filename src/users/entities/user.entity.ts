dc706dbdf5f93362ea0a4e77cff6c0073788d02f
Viktor-Bolotin <145365291+Viktor-Bolotin@users.noreply.github.com>
2025-12-16 01:04:05 +0300

    Добавлен entity для User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,73 @@
-export class User {}
+import { IsDefined, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+enum UserRole {
+  user = 'USER',
+  admin = 'ADMIN',
+}
+
+enum UserGender {
+  male = 'мужской',
+  female = 'женский',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @IsDefined()
+  @MinLength(2)
+  name: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  @IsDefined()
+  email: string;
+
+  @Column()
+  @IsDefined()
+  @MinLength(8)
+  password: string;
+
+  @Column()
+  about: string;
+
+  @Column()
+  @IsDefined()
+  @IsNotEmpty()
+  birthdate: Date;
+
+  @Column()
+  city: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserGender,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  gender: string;
+
+  @Column()
+  avatar: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.user,
+  })
+  role: string;
+
+  @Column()
+  refreshToken: string;
+
+  // Добавить связи с другими entity
+  @Column()
+  skills: string;
+  @Column()
+  wantToLearn: string;
+  @Column()
+  favoriteSkills: string;
+}
