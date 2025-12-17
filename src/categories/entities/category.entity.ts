307586bbdddd002cf9cbe3be44fa2b9fdf6989c7
Elena Romanova <romanova.bim@gmail.com>
2025-12-17 18:47:56 +0500

    описать сущность categories

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,20 @@
-export class Category {}
+import { IsDefined, IsNotEmpty, Length } from "class-validator";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @IsDefined()
+  @IsNotEmpty()
+  @Length(2, 100)
+  name: string;
+
+  @ManyToOne(() => Category, category => category.children, { nullable: true })
+  parent: Category | null;
+
+  @OneToMany(() => Category, category => category.children)
+  children: Category[];
+}
