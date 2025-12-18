63bdb770f5e35c99741ec0d5e1c84be155165a04
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-12-19 02:53:16 +0500

    feat(files): реализовать загрузку изображений с валидацией и сохранением через Multer

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  createFileResponse(file: Express.Multer.File) {
+    // Формируем ответ для клиента
+    return {
+      url: `/uploads/${file.filename}`,
+      name: file.originalname,
+      mimeType: file.mimetype,
+      size: file.size,
+    };
+  }
+}
