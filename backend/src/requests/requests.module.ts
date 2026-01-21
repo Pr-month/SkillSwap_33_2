8c613249cd20837c51da26ef5db62dbc22ff3429
Igor Bocharov <igor.bocharov56@yandex.ru>
2026-01-21 22:33:30 +0500

    feat(requests): импортировать NotificationModule для отправки уведомлений

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -5,9 +5,13 @@ import { RequestsController } from './requests.controller';
 import { Request } from './entities/request.entity';
 import { Skill } from '../skills/entities/skill.entity';
 import { User } from '../users/entities/user.entity';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request, User, Skill])],
+  imports: [
+    TypeOrmModule.forFeature([Request, User, Skill]),
+    NotificationModule,
+  ],
   controllers: [RequestsController],
   providers: [RequestsService],
   exports: [RequestsService],
