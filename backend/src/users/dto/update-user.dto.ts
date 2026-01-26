d0b33955a744f03aca6cb569cc33e0fe5eb1ba69
Viktor-Bolotin <145365291+Viktor-Bolotin@users.noreply.github.com>
2026-01-26 21:32:50 +0300

    Изменён UpdateUserDto

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,59 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsDate,
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+import { GenderOption, UserRole } from '../enums';
+import { Type } from 'class-transformer';
+import { UpdateSkillDto } from 'src/skills/dto/update-skill.dto';
+import { UpdateCategoryDto } from 'src/categories/dto/update-category.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  about?: string;
+
+  @IsOptional()
+  @IsDate()
+  birthdate?: Date;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  gender?: GenderOption;
+
+  @IsOptional()
+  avatar?: string;
+
+  @IsOptional()
+  role?: UserRole;
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateSkillDto)
+  skills?: UpdateSkillDto[];
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateCategoryDto)
+  wantToLearn?: UpdateCategoryDto[];
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateSkillDto)
+  favoriteSkills?: UpdateSkillDto[];
+}
