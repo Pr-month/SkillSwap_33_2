de9cf07883910a98f317795f62e74b87098061f0
Viktor-Bolotin <145365291+Viktor-Bolotin@users.noreply.github.com>
2026-01-26 22:27:09 +0300

    Удалён хардкод в login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { User } from '../users/entities/user.entity';
@@ -19,6 +19,8 @@ export class AuthService {
     private readonly configService: ConfigService,
     @InjectRepository(RefreshToken)
     private refreshTokensRepository: Repository<RefreshToken>,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async register(registerDto: RegisterDto) {
@@ -61,9 +63,17 @@ export class AuthService {
   }
 
   async login(loginDto: LoginDto): Promise<Tokens> {
-    const user = { id: '1', email: loginDto.email, role: UserRole.USER };
+    const user = await this.usersRepository.findOne({
+      where: { email: loginDto.email },
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        `Пользователь с email ${loginDto.email} не найден`,
+      );
+    }
     //Добавить проверку пароля
-    return this._generateTokens(user as User);
+    return this._generateTokens(user);
   }
 
   async refresh(payload: TJwtPayload): Promise<Tokens> {
