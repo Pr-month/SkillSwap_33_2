19b6c26d3b5879ae9d97050c0a89dc40d364c3fb
Igor Bocharov <igor.bocharov56@yandex.ru>
2026-01-30 00:01:11 +0500

    feat(requests): добавить email-уведомления о статусе заявок (новая, принята, отклонена)

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -6,11 +6,13 @@ import { Request } from './entities/request.entity';
 import { Skill } from '../skills/entities/skill.entity';
 import { User } from '../users/entities/user.entity';
 import { NotificationModule } from '../notification/notification.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Request, User, Skill]),
     NotificationModule,
+    MailModule,
   ],
   controllers: [RequestsController],
   providers: [RequestsService],
